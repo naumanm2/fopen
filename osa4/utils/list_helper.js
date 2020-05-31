ab425ab04e6ec4a8befeeb175ed6a070ef1bd111
naumanm2 <max.naumanen@aalto.fi>
2020-05-31 16:43:42 +0300

    4.5

--- osa4/utils/list_helper.js
+++ osa4/utils/list_helper.js
@@ -6,7 +6,24 @@ const totalLikes = (blogs) => {
   return blogs.reduce((x, y) => x + y.likes, 0)
 }
 
+const mostLikes = (blogs) => {
+  let i = 0
+  const mvp = [0, 0]
+  blogs.forEach(x => {
+    if (x.likes>mvp[0])  {
+        mvp[0] = x.likes
+        mvp[1] = i
+        i++
+      }
+    else {
+      i++;
+    }
+  })
+  return blogs[mvp[1]]
+}
+
 module.exports = {
   dummy,
-  totalLikes
+  totalLikes,
+  mostLikes
 }
