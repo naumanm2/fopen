677e0d172dcee950de3d75659475c252348ccaa0
naumanm2 <max.naumanen@aalto.fi>
2020-07-22 18:44:35 +0300

    5.19

--- osa4/bloglist-frontend/cypress/integration/blog_app.spec.js
+++ osa4/bloglist-frontend/cypress/integration/blog_app.spec.js
@@ -35,4 +35,40 @@ describe('Blog app', function() {
       .should('have.css', 'color', 'rgb(255, 0, 0)')
     })
   })
+
+  describe.only('When logged in', function() {
+    beforeEach(function() {
+      cy.contains('log in').click()
+      cy.get('#username').type('naummax')
+      cy.get('#password').type('mypassword')
+      cy.contains('login').click()
+      cy.contains('new blog').click()
+      cy.get('#title').type('my testing the')
+      cy.get('#author').type('max naum')
+      cy.get('#url').type('www.bo.fi')
+      cy.contains('create').click()
+    })
+
+    it('A blog can be created', function() {
+      cy.get('.addblog').contains('new blog').click()
+      cy.get('#title').type('my beautiful blogpost')
+      cy.get('#author').type('max naum')
+      cy.get('#url').type('www.go.fi')
+      cy.contains('create').click()
+      cy.get('.showOnDefault').should('contain', 'my beautiful blogpost')
+      cy.contains('view').click()
+      cy.get('.doNotShowOnDefault')
+        .should('contain', 'www.go.fi')
+        .should('contain', 'likes 0')
+    })
+
+    it('A blog can be liked', function() {
+      cy.contains('view').click()
+      cy.contains('likes 0')
+      cy.contains('like').click()
+      cy.contains('likes 1')
+
+    })
+  })
+
 })
